afee6e6f51468b46b535163891a244cdf93388ee
陈挺强 <tingqiang.chen@bm9c.com>
2020-07-24 17:36:00 +0800

    富文本

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,7 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '导航', icon: 'dashboard' }
     }]
   },
 
@@ -60,19 +60,19 @@ export const constantRoutes = [
     component: Layout,
     redirect: '/example/table',
     name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
+    meta: { title: '示例', icon: 'el-icon-s-help' },
     children: [
       {
         path: 'table',
         name: 'Table',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        meta: { title: '表格', icon: 'table' }
       },
       {
         path: 'tree',
         name: 'Tree',
         component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
+        meta: { title: '树形', icon: 'tree' }
       }
     ]
   },
@@ -85,7 +85,20 @@ export const constantRoutes = [
         path: 'index',
         name: 'Form',
         component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
+        meta: { title: '表单', icon: 'form' }
+      }
+    ]
+  },
+
+  {
+    path: '/ueditor',
+    component: Layout,
+    children: [
+      {
+        path: 'ueditor',
+        name: 'Ueditor',
+        component: () => import('@/views/ueditor/index'),
+        meta: { title: '富文本编辑器', icon: 'form' }
       }
     ]
   },
